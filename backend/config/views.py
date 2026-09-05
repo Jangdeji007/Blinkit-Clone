ec8a7e41daf7994d4848f1babd6f6cada3f56a5f
karan <karanjangde007@gmail.com>
2026-09-05 11:49:26 +0530

    add cloudinary_configuration

--- backend/config/views.py
+++ backend/config/views.py
@@ -1,5 +1,12 @@
+import os
+
+from django.conf import settings
 from django.http import JsonResponse
 
 
 def health_check(_request):
-    return JsonResponse({'status': 'ok'})
+    return JsonResponse({
+        'status': 'ok',
+        'cloudinary_configured': bool(os.environ.get('CLOUDINARY_URL', '').strip()),
+        'media_storage': settings.STORAGES['default']['BACKEND'],
+    })
